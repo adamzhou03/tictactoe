eaa266a1d2519512daa0fa230714e2ec78b1031e
BrysonCrooks <bcroo098@uottawa.ca>
2024-06-18 16:50:36 -0400

    added win condition and colours

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,18 @@
 var turn = 0; //0 for x's, 1 for o's
+const winStates = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 window.onload = function () {
     initialize();
+    document.getElementById("message").innerHTML = "X's Turn";
 }
 
 
@@ -18,7 +29,7 @@ function initialize() {
             tile.yAxis = y.toString();
             tile.classList = "tile";
             document.getElementById("board").appendChild(tile);
-            tile.set = false;
+            tile.set = 0;
             tile.addEventListener("click", function () {
                 clickHandler(tile.id)
             })
@@ -56,32 +67,60 @@ function initialize() {
         }
     }
 
-
-
 }
 
 function clickHandler(id) { 
     let tile = document.getElementById(id);
-    if (tile.set == false) { //if tile is not already filled out, set box to player's symbol,
+    if (tile.set == 0) { //if tile is not already filled out, set box to player's symbol,
                              //mark box as checked, and change the turn variable to the other player
         if (turn == 0) {
-            tile.innerHTML = "<i class='fa-solid fa-x'></i>";
+            tile.innerHTML = "<i class='fa-solid fa-x' style='color: #ff0000'></i>";
             turn = 1;
+            tile.set = 1;
+            document.getElementById("message").innerHTML = "O's Turn";
         } else {
-            tile.innerHTML = "<i class='fa-regular fa-circle'></i>";
+            tile.innerHTML = "<i class='fa-regular fa-circle' style='color: #0000FF'></i>";
             turn = 0;
+            tile.set = 2;
+            document.getElementById("message").innerHTML = "X's Turn";
         }
-        tile.set = true;
+        
+        checkGameState();
+
     }
 
+    
+}
+
+function checkGameState(){
+    let tiles = document.getElementsByClassName("tile");
+
+    for( let i = 0; i < winStates.length; i++){//if board matches a win state, declare X or O the winner
+        const [x,y,z] = winStates[i];
+        if (tiles[x].set == 1 && tiles[y].set == 1 && tiles[z].set == 1){
+            document.getElementById("message").innerHTML = "X Wins!";
+            tiles[x].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+            tiles[y].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+            tiles[z].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+        }
+        else if (tiles[x].set == 2 && tiles[y].set == 2 && tiles[z].set == 2){
+            document.getElementById("message").innerHTML = "O Wins!";
+            tiles[x].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
+            tiles[y].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
+            tiles[z].innerHTML = "<i class='fa-regular fa-cirlce' style='color: #008000'></i>";
+        }
+
+    }
 }
 
 function resetGame() { //iterate through all tiles and reset their contents, as well as their status
     let tiles = document.getElementsByClassName("tile");
+    document.getElementById("message").innerHTML = "X's Turn";
+    turn = 0;
 
     for (let i = 0; i < tiles.length; i++) {
         tiles[i].innerHTML = '';
-        tiles[i].set = false;
+        tiles[i].set = 0;
     }
 }
 
