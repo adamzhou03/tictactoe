8f14e7c2c936b8a9cd1cbfeca299fbba27be69c0
adamzhou03 <adamzhou03@gmail.com>
2024-07-17 09:01:14 -0400

    added tie functionality

--- gamelogic.php
+++ gamelogic.php
@@ -1,101 +1,157 @@
 <?php
 session_start();
-
-$_SESSION["winstates"] = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+$_SESSION[ 'winstates' ] = [
+    [ 0, 1, 2 ],
+    [ 3, 4, 5 ],
+    [ 6, 7, 8 ],
+    [ 0, 3, 6 ],
+    [ 1, 4, 7 ],
+    [ 2, 5, 8 ],
+    [ 0, 4, 8 ],
+    [ 2, 4, 6 ]
 ];
 
 $tileIds = [
-    0 => "0-0",
-    1 => "0-1",
-    2 => "0-2",
-    3 => "1-0",
-    4 => "1-1",
-    5 => "1-2",
-    6 => "2-0",
-    7 => "2-1",
-    8 => "2-2"
+    0 => '0-0',
+    1 => '0-1',
+    2 => '0-2',
+    3 => '1-0',
+    4 => '1-1',
+    5 => '1-2',
+    6 => '2-0',
+    7 => '2-1',
+    8 => '2-2'
 ];
 
-$q = $_REQUEST["q"];
+if ( isset( $_REQUEST[ 'q' ] ) ) {
+    $q = $_REQUEST[ 'q' ];
 
-if(strlen($q) > 3){//if initializing set of tiles then save empty states
-    $_SESSION["tiles"] = json_decode($q, true);
-    $_SESSION["turn"] = 0;
-    echo "working";
-}
+    if ( $q == 'loaded' ) {
+        // initalize tiles on load.
+        $_SESSION[ 'turn' ] = 0;
+        $_SESSION[ 'tiles' ] = [
+            array( 'xAxis' => 0, 'yAxis' => 0, 'set' => 0 ),
+            array( 'xAxis' => 0, 'yAxis' => 1, 'set' => 0 ),
+            array( 'xAxis' => 0, 'yAxis' => 2, 'set' => 0 ),
+            array( 'xAxis' => 1, 'yAxis' => 0, 'set' => 0 ),
+            array( 'xAxis' => 1, 'yAxis' => 1, 'set' => 0 ),
+            array( 'xAxis' => 1, 'yAxis' => 2, 'set' => 0 ),
+            array( 'xAxis' => 2, 'yAxis' => 0, 'set' => 0 ),
+            array( 'xAxis' => 2, 'yAxis' => 1, 'set' => 0 ),
+            array( 'xAxis' => 2, 'yAxis' => 2, 'set' => 0 )
+        ];
+    } else if ( strlen( $q ) === 0 ) {
+        checkEndGame();
 
-else if(strlen($q) === 0){
-    checkWin();
-}
-
-else{//gets tile id(0 to 8)
-    for ($x = 0; $x <= 8; $x++){
-        if ($tileIds[$x] === $q){
-            fillTile($x);
+    } else if ( $q == 'reset' ) {
+        resetGame();
+    } else {
+        //gets tile id( 0 to 8 )
+        for ( $x = 0; $x <= 8; $x++ ) {
+            if ( $tileIds[ $x ] === $q ) {
+                fillTile( $x );
+            }
         }
-    } 
+
+    }
 }
 
-function fillTile($id){//if tile is empty then fill tile with X or O
-    $tile = $_SESSION["tiles"][$id];
-    if ($tile["set"] === 0) {
+function fillTile( $id ) {
+    //if tile is empty then fill tile with X or O
 
-        if ($_SESSION["turn"] === 0){
-            $_SESSION["tiles"][$id]["set"] = 1;
-            $_SESSION["turn"] = 1;
+    $tile = $_SESSION[ 'tiles' ][ $id ];
+    if ( $tile[ 'set' ] === 0 ) {
+        if ( $_SESSION[ 'turn' ] === 0 ) {
+            $_SESSION[ 'tiles' ][ $id ][ 'set' ] = 1;
+            $_SESSION[ 'turn' ] = 1;
             echo "O's Turn";
-        } 
-        else if ($_SESSION["turn"] === 1) {
-            $_SESSION["tiles"][$id]["set"] = 2;
-            $_SESSION["turn"] = 0;
+            exit;
+        } else if ( $_SESSION[ 'turn' ] === 1 ) {
+            $_SESSION[ 'tiles' ][ $id ][ 'set' ] = 2;
+            $_SESSION[ 'turn' ] = 0;
             echo "X's Turn";
+            exit;
         }
+    } else {
+        echo '';
+        exit;
     }
+
 }
 
-function checkWin(){
-    for($i = 0; $i < count($_SESSION["winstates"]); $i++){
-        $win = $_SESSION["winstates"][$i];
-        $x = $_SESSION["tiles"][$win[0]]["set"];
-        $y = $_SESSION["tiles"][$win[1]]["set"];
-        $z = $_SESSION["tiles"][$win[2]]["set"];
-        if ($x == 1 && $y == 1 && $z == 1){
-            $response = [
-                "X Wins!",
-                $win[0],
-                $win[1],
-                $win[2]
-            ];
-            for ($w = 0; $w < count($_SESSION["tiles"]); $w++){
-                $_SESSION["tiles"][$w]["set"] = 4;
+function checkEndGame() {
+    if ( $_SESSION[ 'turn' ] < 2 ) {
+        for ( $i = 0; $i < count( $_SESSION[ 'winstates' ] );
+        // check if any winstates are filled by one kind of mark.
+        $i++ ) {
+            $win = $_SESSION[ 'winstates' ][ $i ];
+            $x = $_SESSION[ 'tiles' ][ $win[ 0 ] ][ 'set' ];
+            $y = $_SESSION[ 'tiles' ][ $win[ 1 ] ][ 'set' ];
+            $z = $_SESSION[ 'tiles' ][ $win[ 2 ] ][ 'set' ];
+            if ( $x == 1 && $y == 1 && $z == 1 ) {
+                $response = [
+                    'X Wins!',
+                    $win[ 0 ],
+                    $win[ 1 ],
+                    $win[ 2 ]
+                ];
+                for ( $w = 0; $w < count( $_SESSION[ 'tiles' ] );
+                $w++ ) {
+                    $_SESSION[ 'tiles' ][ $w ][ 'set' ] = 4;
+                }
+                $_SESSION[ 'turn' ] = 0;
+                echo json_encode( $response );
+                $_SESSION[ 'turn' ] = 3;
+                // make turn 3 so no more moves can be made.
+                exit;
+            } else if ( $x == 2 && $y == 2 && $z == 2 ) {
+                $response = [
+                    'O Wins!',
+                    $win[ 0 ],
+                    $win[ 1 ],
+                    $win[ 2 ]
+                ];
+                for ( $w = 0; $w < count( $_SESSION[ 'tiles' ] );
+                $w++ ) {
+                    $_SESSION[ 'tiles' ][ $w ][ 'set' ] = 4;
+                }
+                $_SESSION[ 'turn' ] = 0;
+                echo json_encode( $response );
+                $_SESSION[ 'turn' ] = 3;
+                // make turn 3 so no more moves can be made.
+                exit;
             }
-            $_SESSION["turn"] = 0;
-            echo json_encode($response);
+
         }
-        else if ($x == 2 && $y == 2 && $z == 2){
-            $response = [
-                "O Wins!",
-                $win[0],
-                $win[1],
-                $win[2]
-            ];
-            for ($w = 0; $w < count($_SESSION["tiles"]); $w++){
-                $_SESSION["tiles"][$w]["set"] = 4;
-            }
-            $_SESSION["turn"] = 0;
-            echo json_encode($response);
+
+        if (
+            // if all tiles are set and no win, then there is a tie.
+            $_SESSION[ 'tiles' ][ 0 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 1 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 2 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 3 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 4 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 5 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 6 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 7 ][ 'set' ] != 0 &&
+            $_SESSION[ 'tiles' ][ 8 ][ 'set' ]
+        ) {
+            echo 'tie';
+            $_SESSION[ 'turn' ] = 3;
+            exit;
+        } else {
+            echo '';
         }
-        
     }
+
 }
 
+function resetGame() { // reset game.
+    for ( $w = 0; $w < count( $_SESSION[ 'tiles' ] );
+    $w++ ) {
+        $_SESSION[ 'tiles' ][ $w ][ 'set' ] = 0;
+    }
+    $_SESSION[ 'turn' ] = 0;
+}
 
 ?>
--- tictactoe.js
+++ tictactoe.js
@@ -1,43 +1,30 @@
-
 window.onload = function () {
     initialize();
     document.getElementById("message").innerHTML = "X's Turn";
-
-    //send array of tiles to server
-    let tiles = document.getElementsByClassName("tile");
-    tiles = JSON.stringify(tiles);
-    
     var req = new XMLHttpRequest();
-
-    req.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            console.log(this.responseText);
-        }
-    }
-
-    req.open("GET", "index.php?q=" + tiles, true);
+    req.open("GET", "gamelogic.php?q=" + "loaded", true); // set values for tiles after load.
     req.send();
-    
 }
 
-function tile_request_handler(tiles){
+function tile_request_handler(tiles) { // fill tile if empty, and change turn.
     let tile = document.getElementById(tiles);
     var req = new XMLHttpRequest();
 
-    req.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            if (this.responseText === "O's Turn"){
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) { // once request is processed, change turn.
+            if (this.responseText === "O's Turn") {
                 document.getElementById("message").innerHTML = this.responseText;
                 tile.innerHTML = "<i class='fa-solid fa-x' style='color: #ff0000'></i>";
             }
-            else if (this.responseText === "X's Turn"){
+            else if (this.responseText === "X's Turn") {
                 document.getElementById("message").innerHTML = this.responseText;
                 tile.innerHTML = "<i class='fa-regular fa-circle' style='color: #0000FF'></i>";
             }
+            checkGameState(); // check for win or tie.
         }
     }
 
-    req.open("GET", "index.php?q=" + tiles, true);
+    req.open("GET", "gamelogic.php?q=" + tiles, true);
     req.send();
 }
 
@@ -54,7 +41,6 @@ function initialize() {
             tile.yAxis = y.toString();
             tile.classList = "tile";
             document.getElementById("board").appendChild(tile);
-            tile.set = 0;
             tile.addEventListener("click", function () {
                 clickHandler(tile.id)
             })
@@ -96,109 +82,54 @@ function initialize() {
 
 function clickHandler(id) {
     tile_request_handler(id);
-    checkGameState();
-    // document.getElementById("message").innerHTML = fill[0];
-    // tile.innerHTML = fill[1];
-
-    // let tile = document.getElementById(id);
-    // if (tile.set == 0) { //if tile is not already filled out, set box to player's symbol,
-    //                          //mark box as checked, and change the turn variable to the other player
-    //     if (turn == 0) {
-    //         tile.innerHTML = "<i class='fa-solid fa-x' style='color: #ff0000'></i>";
-    //         turn = 1;
-    //         tile.set = 1;
-    //         document.getElementById("message").innerHTML = "O's Turn";
-    //     } else {
-    //         tile.innerHTML = "<i class='fa-regular fa-circle' style='color: #0000FF'></i>";
-    //         turn = 0;
-    //         tile.set = 2;
-    //         document.getElementById("message").innerHTML = "X's Turn";
-    //     }
-        
-  
-
-    // }
-
-    
 }
 
-function checkGameState(){
+function checkGameState() { // checks for win or tie.
     var req = new XMLHttpRequest();
 
-    req.onreadystatechange = function(){
-        if (this.readyState == 4 && this.status == 200){
-            var winMsg = JSON.parse(this.responseText);
-            document.getElementById("message").innerHTML = winMsg[0];
-            let tiles = document.getElementsByClassName("tile");
-            if (winMsg[0] == "X Wins!"){
-                tiles[winMsg[1]].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
-                tiles[winMsg[2]].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
-                tiles[winMsg[3]].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText == "tie") {
+                document.getElementById("message").innerHTML = "Tie!";
             }
-            else if (winMsg[0] == "O Wins!"){
-                tiles[winMsg[1]].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
-                tiles[winMsg[2]].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
-                tiles[winMsg[3]].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
+            else if (this.responseText != "") { // if not tie or no win, parse win message. Contains 
+                // winner and winning marks.
+                var winMsg = JSON.parse(this.responseText);
+                document.getElementById("message").innerHTML = winMsg[0];
+                let tiles = document.getElementsByClassName("tile");
+                if (winMsg[0] == "X Wins!") { // make winning marks green.
+                    tiles[winMsg[1]].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+                    tiles[winMsg[2]].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+                    tiles[winMsg[3]].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
+                }
+                else if (winMsg[0] == "O Wins!") { // make winning marks green.
+                    tiles[winMsg[1]].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
+                    tiles[winMsg[2]].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
+                    tiles[winMsg[3]].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
+                }
             }
+
         }
     }
 
-    req.open("GET", "index.php?q=" + "", true);
+    req.open("GET", "gamelogic.php?q=" + "", true);
     req.send();
-    // let tiles = document.getElementsByClassName("tile");
 
-//     for( let i = 0; i < winStates.length; i++){//if board matches a win state, declare X or O the winner
-//         const [x,y,z] = winStates[i];
-//         if (tiles[x].set == 1 && tiles[y].set == 1 && tiles[z].set == 1){
-//             document.getElementById("message").innerHTML = "X Wins!";
-//             tiles[x].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
-//             tiles[y].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
-//             tiles[z].innerHTML = "<i class='fa-solid fa-x' style='color: #008000'></i>";
-//             for (let i = 0; i < tiles.length; i++) {
-//                 tiles[i].set = 4;
-//             }
-//             return;
-//         }
-//         else if (tiles[x].set == 2 && tiles[y].set == 2 && tiles[z].set == 2){
-//             document.getElementById("message").innerHTML = "O Wins!";
-//             tiles[x].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
-//             tiles[y].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
-//             tiles[z].innerHTML = "<i class='fa-regular fa-circle' style='color: #008000'></i>";
-//             for (let i = 0; i < tiles.length; i++) {
-//                 tiles[i].set = 4;
-//             }
-//             return;
-//         }
-//         else{
-//             checkTie();
-//         }
-
-//     }
 }
 
-// function checkTie(){
-//     let tiles = document.getElementsByClassName("tile");
+function resetGame() { // reset game.
+    document.getElementById("message").innerHTML = "X's Turn";
 
-//     for (let i = 0; i < tiles.length; i++) {
-//         if(tiles[i].set == 0){
-//             return;
-//         }
-//     }
-//     document.getElementById("message").innerHTML = "Tie";
+    let tiles = document.getElementsByClassName("tile");
+    for (let i = 0; i < tiles.length; i++) {
+        tiles[i].innerHTML = "";
+        tiles[i].set = 0;
+    }
 
-// }
-
-// function resetGame() { //iterate through all tiles and reset their contents, as well as their status
-//     let tiles = document.getElementsByClassName("tile");
-//     document.getElementById("message").innerHTML = "X's Turn";
-//     turn = 0;
-
-//     for (let i = 0; i < tiles.length; i++) {
-//         tiles[i].innerHTML = '';
-//         tiles[i].set = 0;
-//     }
-// }
-
+    var req = new XMLHttpRequest();
+    req.open("GET", "gamelogic.php?q=" + "reset", true);
+    req.send();
+}
 
 
 
